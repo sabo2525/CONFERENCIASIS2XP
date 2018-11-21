d66942bd8560c2cdfc5c12ba845efcf57f73fa44
luis9448286 <luis.jorge.herbasg@gmail.com>
2018-11-21 01:05:28 -0400

    Add files via upload

---
+++ src/DB/Conexion.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DB;
+
+import java.sql.Connection;
+
+
+
+/**
+ *
+ * @author pc
+ */
+public class Conexion {
+    
+   
+    public Conexion(){
+    
+    
+
+}
+
+    boolean isConectado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+
+    Connection getConexion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
