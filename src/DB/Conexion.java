adbee75a3f6477612bd2f9d40da17d290204a74c
luis9448286 <luis.jorge.herbasg@gmail.com>
2018-11-21 01:30:25 -0400

    Update Conexion.java

--- src/DB/Conexion.java
+++ src/DB/Conexion.java
@@ -6,29 +6,54 @@
 package DB;
 
 import java.sql.Connection;
-
-
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author pc
+ * @author LUIS
  */
 public class Conexion {
     
-   
+    private Connection conexion;
     public Conexion(){
-    
-    
+    conexion=null;
+        //********************************************** 
+// Se definen datos para la conexion a la base de datos* 
+//********************************************** 
+String driver = "org.postgresql.Driver"; 
+String connectString = "jdbc:postgresql://localhost:5432/BD_Xp"; 
+// ACA PONE DONDE ESTA TU BASE DE DATOS 
+String user = "postgres"; 
+String password = "herbas"; 
 
+try { 
+//************************************************************************* 
+// Se realiza la conexión a la Base de Datos tomando los datos anteriormente definidos* 
+//************************************************************************* 
+Class.forName(driver); 
+ conexion = DriverManager.getConnection(connectString, user, password); 
+    
+    
+}       catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
 }
 
-   public boolean isConectado() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+public boolean isConectado(){
+     boolean valid = false;
+        try {
+            valid = conexion.isValid(50000);
+           System.out.println(valid ? "TEST OK" : "TEST FAIL");  
 
-    
-
-  public  Connection getConexion() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            return valid;
     }
+ public Connection getConexion(){
+        return conexion;
+}
 }
