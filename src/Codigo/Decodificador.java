18d09a0748aad76a5a2835e844c4ca5859571dfb
Revollito <Revollito@192.168.182.1>
2018-11-22 22:14:26 -0400

    interfaz y codigo de decodificacion qr

---
+++ src/Codigo/Decodificador.java
@@ -0,0 +1,38 @@
+package Codigo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Revollito
+ */
+public class Decodificador {
+    public  String decodificarQr(File qrCodeimage) throws IOException {//busca una imagen en el directorio y decodifica en un string
+        BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            return "noQr";
+        }
+    }
+}
